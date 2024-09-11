0fed5ae79ee4610055715c8d0a316c92d65878e4
tjrdbfl <tjrdbfl123@gmail.com>
2024-09-11 12:41:56 +0900

    Brute force Algorithm

--- week1/maxsum/maxsum1.py
+++ week1/maxsum/maxsum1.py
@@ -1,23 +1,15 @@
 import sys
 
-def festival1(N,L,cost):
-    psum=cost[:]+[0]
-    for i in range(N):
-        psum[i]+=psum[i-1]
-
-    min_cost=float('inf')
-    for i in range(N):
-        for j in range(i,N):
-            if j-i+1>=L:
-                avg_cost=(psum[j]-psum[i-1])/(j-i+1)
-                min_cost=min(avg_cost, min_cost)
-    
-    return min_cost
+def maxsum(n,A):
+    curs,max=0,0
+    for i in range(n):
+        curs=max(curs,0)+A[i]
+        maxs=mas(maxs,curs)
+    return maxs
 
 input=sys.stdin.readline
 for _ in range(int(input())):
-    N,L=map(int,input().split())
-
-    cost=list(map(int,input().split()))
-
-    print(festival1(N,L,cost))
+    N=int(input())
+    A=list(map(int,input().split()))
+    
+    print(maxsum(N,A))
---
+++ week2/clock/clock1.py
@@ -0,0 +1,43 @@
+import sys
+linked={
+    0:[0,1,2],
+    1:[3,7,9,11],
+    2:[4,10,14,15],
+    3:[0,4,5,6,7],
+    4:[6,7,8,10,12],
+    5:[0,2,14,15],
+    6:[3,14,15],
+    7:[4,5,7,14,15],
+    8:[1,2,3,4,5],
+    9:[3,4,5,9,13],
+}
+sequence=[
+    (8,linked[4]),
+    (11,linked[1]),
+    (13,linked[9]),
+    (6,linked[3]),
+    (10,linked[2]),
+    (7,linked[7]),
+    (4,linked[8]),
+    (1,linked[0]),
+    (3,linked[6]),
+    (0,linked[5]),
+]
+    
+
+def clocksync(clocks):
+    ret=0
+    for clock,linked_clock in sequence:
+        cnt=(4-clocks[clock])%4
+        for other in linked_clock:
+            clocks[other]=(clocks[other]+cnt)%4
+        ret += cnt
+    return -1 if any(clocks) else ret
+
+INF=31
+input=sys.stdin.readline
+for _ in range(int(input())):
+    clocks=[(int(x)//3)%4 for x in input().split()]
+    mincnt=clocksync(clocks)
+    print(mincnt if mincnt<INF else -1)
+
